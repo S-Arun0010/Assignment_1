26aef62774ad8873a9a8067f648a9cc7561f0c5e
S-Arun0010 <123442099+S-Arun0010@users.noreply.github.com>
2023-01-25 20:43:00 +0530

    Update student.java

--- New_Bank/src/main/java/student.java
+++ New_Bank/src/main/java/student.java
@@ -1,27 +1,32 @@
 import java.util.Scanner;
-public class Student {
+
+public class student {
     String name;
     char gradelevel;
     double gp;
+
     student(String n, char g, double gpa) {
         name = n;
         gradelevel = g;
         gp = gpa;
     }
+
     public void updategpa(double gpa) {
         gp = gpa;
     }
+
     public String details() {
         return (name + " has a " + Double.toString(gp) + " GPA");
     }
+
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        student Student = new student("Arun", 'A', 8.73);
-        System.out.println("Enter the GPA :");
+        student student = new student("Arun", 'A', 8.73);
+        logger.log("Enter the GPA :");
         double gpa = sc.nextDouble();
-        Student.updategpa(gpa);
-        String s = Student.details();
-        System.out.println(s);
+        student.updategpa(gpa);
+        String s = student.details();
+        logger.log(s);
     }
 }
 
