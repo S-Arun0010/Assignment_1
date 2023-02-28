4b4669133c3cc1b6e2264bbf5362d60224118e3f
S-Arun0010 <123442099+S-Arun0010@users.noreply.github.com>
2023-02-28 12:16:48 +0530

    Update Student.java

--- New_Bank/src/main/java/studentassignment/Student.java
+++ New_Bank/src/main/java/studentassignment/Student.java
@@ -1,29 +1,49 @@
 package studentassignment;
 import java.util.Scanner;
-import java.util.logging.Logger;
+import java.io.FileDescriptor;
+import java.io.FileOutputStream;
+import java.io.PrintStream;
+
 public class Student {
+	
     String name;
     char gradelevel;
-    double gp;
-    static Logger l= Logger.getLogger("com.api.jar");
-    Student(String n, char g, double gpa) {
-        name = n;
-        gradelevel = g;
-        gp = gpa;
+    double gpa;   
+    static PrintStream print = new PrintStream (new FileOutputStream(FileDescriptor.out));
+    
+    Student(String name, char grade, double gpa) {
+        this.name = name;
+        this.gradelevel = grade;
+        this.gpa = gpa;
     }
-    public void updategpa(double gpa) {
-        gp = gpa;
+    
+    public void updategpa(double newgpa) {
+       gpa = newgpa;
+    	
     }
+    
     public String details() {
-        return (name + " has a " + Double.toString(gp) + " GPA");
+        return (name + " has a " + gpa + " GPA");
     }
+    
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        Student student = new Student("Arun", 'A', 8.73);
-        l.info("Enter the GPA :");
-        double gpa = sc.nextDouble();
-        student.updategpa(gpa);
+    	
+        Scanner sc = new Scanner(System.in);        
+        print.println("Enter the Student Name:");
+        String name = sc.nextLine();
+        print.println("Enter the Student Grade:");
+        char grade = sc.next().charAt(0);
+        print.println("Enter the Student GPA:");
+        double gpa = sc.nextDouble(); 
+        
+        Student student = new Student(name, grade, gpa);
+        
+        print.println("Enter the new GPA :");
+        double newgpa = sc.nextDouble();
+        student.updategpa(newgpa);
         String s = student.details();
-        l.info(s);
+        print.println(s);  
+        
+        }
     }
-}
+
